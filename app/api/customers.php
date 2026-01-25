18bdc316f2380c8f5057abe2b5d1d2ed8963b773
37265 <37265@protonmail.com>
2026-01-25 18:43:54 +0100

    Test

--- app/api/customers.php
+++ app/api/customers.php
@@ -1,14 +1,2 @@
 <?php
-header('Content-Type: application/json');
-
-$pdo = new PDO(
-  'mysql:host=mariadb;dbname=InAudible;charset=utf8mb4',
-  'demo_user',
-  'password',
-  [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-  ]
-);
-
-$stmt = $pdo->query('SELECT id, name FROM Customer');
-echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
+echo "PHP is aliver";
