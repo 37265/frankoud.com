b87dec9884977803eb9164ff2ad019e548e7ca5b
37265 <37265@protonmail.com>
2026-01-26 12:28:19 +0100

    Add query to result

--- app/api/customers.php
+++ app/api/customers.php
@@ -9,7 +9,16 @@ $pdo = new PDO(
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
   ]
 );
-$sql = 'SELECT id, first_name, last_name FROM Customer';
 
-$stmt = $pdo->query($sql); 
-echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
+$sql = 
+'SELECT id, first_name, last_name 
+FROM Customer';
+
+$stmt = $pdo->query($sql);
+$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$response = [
+  "query" => $sql,
+  "results" => $results
+];
+
+echo json_encode(value: $data);
